1b0ea5694a9717606129d80b7e3410f9e89f4ecb
jocarino <joao.goncalo.veloso@gmail.com>
2024-04-29 19:46:35 +0100

    feat: local development is ready

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <main className=""> test</main>;
+  return (
+    <main className="w-full h-full bg-pink">
+      <div className="">asdfg</div>
+    </main>
+  );
 }
